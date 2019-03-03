53b40cf57d0c8561875b3af41e51607082f8dceb
eugene-galkine <eugene.galkine@gmail.com>
2019-03-02 19:43:41 -0600

    added more tests for conversion

--- src/eg/tests/ConvertToFromBytes.java
+++ src/eg/tests/ConvertToFromBytes.java
@@ -16,10 +16,58 @@ public class ConvertToFromBytes {
 	}
 	
 	@Test
+	public void testIntConversionMin() {
+		int actual = Integer.MIN_VALUE;
+		byte[] data = ByteArrayUtils.appendInt(new byte[4], 0, actual);
+		assertEquals(ByteArrayUtils.parseInt(data, 0), actual);
+	}
+	
+	@Test
+	public void testIntConversionMax() {
+		int actual = Integer.MAX_VALUE;
+		byte[] data = ByteArrayUtils.appendInt(new byte[4], 0, actual);
+		assertEquals(ByteArrayUtils.parseInt(data, 0), actual);
+	}
+	
+	@Test
 	public void testFloatConversion() {
 		float actual = 8.975f;
 		byte[] data = ByteArrayUtils.appendFloat(new byte[4], 0, actual);
 		assertTrue(ByteArrayUtils.parseFloat(data, 0) == actual);
 	}
+	
+	@Test
+	public void testFloatConversionMin() {
+		float actual = Float.MIN_VALUE;
+		byte[] data = ByteArrayUtils.appendFloat(new byte[4], 0, actual);
+		assertTrue(ByteArrayUtils.parseFloat(data, 0) == actual);
+	}
 
+	@Test
+	public void testFloatConversionMax() {
+		float actual = Float.MAX_VALUE;
+		byte[] data = ByteArrayUtils.appendFloat(new byte[4], 0, actual);
+		assertTrue(ByteArrayUtils.parseFloat(data, 0) == actual);
+	}
+	
+	@Test
+	public void testFloatConversionInfinity() {
+		float actual = Float.POSITIVE_INFINITY;
+		byte[] data = ByteArrayUtils.appendFloat(new byte[4], 0, actual);
+		assertTrue(ByteArrayUtils.parseFloat(data, 0) == actual);
+	}
+	
+	@Test
+	public void testFloatConversionNegInfinity() {
+		float actual = Float.NEGATIVE_INFINITY;
+		byte[] data = ByteArrayUtils.appendFloat(new byte[4], 0, actual);
+		assertTrue(ByteArrayUtils.parseFloat(data, 0) == actual);
+	}
+	
+	@Test
+	public void testFloatConversionSmallNeg() {
+		float actual = -0.143673f;
+		byte[] data = ByteArrayUtils.appendFloat(new byte[4], 0, actual);
+		assertTrue(ByteArrayUtils.parseFloat(data, 0) == actual);
+	}
 }
